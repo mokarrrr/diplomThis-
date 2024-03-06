d2e827e52f89c77a2fc2f6f38a7e233c79b54087
alexs <Alex.senin.95@mail.ru>
2024-03-06 19:06:43 +0400

    сделал двойное нажатие на кнопку избранное

--- diplom/Models/MainContext.cs
+++ diplom/Models/MainContext.cs
@@ -38,9 +38,9 @@ using Microsoft.EntityFrameworkCore;
     {
         //optionsBuilder.UseSqlServer(@"Data Source=192.168.227.12;User ID=user04;Password=04;database=1myay;TrustServerCertificate=True");
 
-        //optionsBuilder.UseSqlServer(@"Data Source=DESKTOP-SD2NSU5\MSSQLSERVER05;User ID=sa;Password=12345;database=this2;TrustServerCertificate=True");
+        optionsBuilder.UseSqlServer(@"Data Source=DESKTOP-SD2NSU5\MSSQLSERVER05;User ID=sa;Password=12345;database=this3;TrustServerCertificate=True");
 
-        optionsBuilder.UseSqlServer("Data Source=DESKTOP-3FU748J;Database=misha;Integrated Security = sspi; Encrypt=False;");
+        //optionsBuilder.UseSqlServer("Data Source=DESKTOP-3FU748J;Database=misha;Integrated Security = sspi; Encrypt=False;");
     }
     public MainContext()
     {
