6754a7f527ae6ba4a840cabe46957c7c618ab4fc
alexs <Alex.senin.95@mail.ru>
2024-03-31 22:57:40 +0400

    папап

--- diplom/Models/_order.cs
+++ diplom/Models/_order.cs
@@ -15,7 +15,7 @@ namespace diplom.Models
         public string order_city { get; set;}
         public DateTime order_date { get; set;}
         public string order_address { get; set;}
-        public string order_commentary { get; set;}
+        public string? order_commentary { get; set;}
         public int _user_id { get; set;}
 
         public int OrderSum { get; set;}
